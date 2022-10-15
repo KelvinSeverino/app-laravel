a7fe937f27c90bada4238183e7b36313e849abf7
Kelvin Severino <kseveri@outlook.com>
2022-10-15 09:06:37 -0300

    Adição de Templates Blade

--- source/laravel/resources/views/users/index.blade.php
+++ source/laravel/resources/views/users/index.blade.php
@@ -1,3 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
 <h1>Listagem de Usuários</h1>
 
 <ul>
@@ -5,10 +10,9 @@
     @foreach ($users as $user)    
 
         {{-- Essa Tag serve para passar os dados para o HTML de maneira segura--}}
-        <li>{{ $user->name }}</li>
-        <li>{{ $user->email }}</li>
-        | <a href="{{ route('users.show', ['id' => $user->id ]) }}">Ver</a>
+        <li>{{ $user->name }} - {{ $user->email }} | <a href="{{ route('users.show', ['id' => $user->id ]) }}">Detalhes</a></li> 
 
         {{-- {!! !!}  Essa tag também faz o mesmo que {{ }}, EXCETO que possui FALHAS NA SEGURANÇA contra XSS --}}
     @endforeach
 </ul>
+@endsection
--- source/laravel/resources/views/users/show.blade.php
+++ source/laravel/resources/views/users/show.blade.php
@@ -1,6 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Lista do usuário')
+
+@section('content')
 <h1>Lista do usuário {{ $user->name }}</h1>
 
 <ul>
     <li>{{ $user->name }}</li>
     <li>{{ $user->email }}</li>
 </ul>
+@endsection
