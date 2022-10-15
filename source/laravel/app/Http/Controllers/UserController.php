ac5ace7140aa43481497814d4267da0214d3c0f1
Kelvin Severino <kseveri@outlook.com>
2022-10-15 09:35:28 -0300

    Cadastro de Usuarios

--- source/laravel/app/Http/Controllers/UserController.php
+++ source/laravel/app/Http/Controllers/UserController.php
@@ -31,8 +31,34 @@ class UserController extends Controller
         return view('users.create');
     }
 
-    public function store()
+    public function store(Request $request) //Request $request é funcao nativa que pega todos os dados vindo do form
     {
-        dd('cadastrando');
+        //Informa quais inputs serao considerados
+        /*dd($request->only([
+            'name',
+            'email',
+            'password'
+        ]));*/
+
+        //Pega todos os inputs
+        //dd($request->all());
+
+        //Gravacao da maneira tradicional
+        /*
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        */
+
+        //Gravacao de maneira mais pratica e otimizada
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        
+        User::create($data);
+
+        //Redireciona para Listagem
+        return redirect()->route('users.index');
     }
 }
